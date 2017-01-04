21d0b9568b3bce81d043abe6b6718d7df180af05
Alex Dong <me@alexdong.com>
2017-01-04 22:36:19 +1300

    Support webpack:/// format

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 This is my personal iTerm semantic history script. It handles any text that I command-click on in iTerm2.
+It opens paths in PyCharm and bring PyCharm to front
 
-Functions so far:
+For now, it supports the following constructs:
 
-* Open paths in PyCharm and bring PyCharm to front
+* karma output like `at Context.<anonymous> (webpack:///frontend_tests/models/test_order_item.js:186:11 <-
+  frontend_tests/index.js:32906:23)`
+
+* py.test output
+
 
 ### To Install: ###
 
 1. Fork and clone the repo.
-2. Go to Preferences -> Profiles -> Advanced -> Semantic History -> Run command ... and enter something like:
+2. Go to Preferences -> Profiles -> Advanced -> Semantic History -> Always run command ... and enter something like:
 
-		/usr/local/bin/python3 ~/Projects/iterm_semantic_history/script.py "\1" "\2" "\5"
+    /usr/local/bin/python3 ~/Projects/iterm_semantic_history/script.py "\1" "\2" "\5"
     
--- script.py
+++ script.py
@@ -2,12 +2,46 @@ import sys
 from subprocess import run
 import os
 
-filename, line_number, pwd = sys.argv[1:]
-
 pycharm_path = "/Applications/PyCharm.app/Contents/MacOS/pycharm"
 
-# strip quotes before and after file name
-filename = filename.strip(':"\'')
+"""
+There is no good way for this script to report any errors.
+Whenever something isn't quite right, it just quietly crashed.
+Leaving nothing for us to debug. So the debug strategy here
+is quite arcane. We manually append log messages to a file.
+
+To help debugging, run the following command in another window.
+
+    tail -f /tmp/script.log
+
+"""
+
+def debug(s):
+    fp = open('/tmp/script.log', 'w+')
+    fp.writelines([s + os.linesep])
+    fp.close()
+
+filename, line_number, pwd = sys.argv[1:]
+
+# strip quotes, backslash, () and [] before and after file name
+filename = filename.strip(':"\\\'').strip('[(').strip('])')
+
+# If filename starts with webpack:///
+if filename.startswith('http:') or filename.startswith('https:'):
+    run(["open " + filename])
+
+
+if filename.startswith('webpack:///'):
+    filename = filename[len('webpack:///'):]
+
+# If the filename is like "file.js:line:column", parse out the line number
+if len(filename.split(':')) > 1:
+    parts = filename.split(':')
+    filename = parts[0]
+    if parts[1].isdigit():
+        line_number = parts[1]
+
+#debug(filename)
 
 # absolute path
 filename = os.path.join(pwd, filename)
@@ -21,10 +55,7 @@ linenumber = "--line %s" % line_number
 # 2. `shell=True` is required. Otherwise, it'll confuse the pycharm
 #    command which is a thin wrapper.
 cmd = "%s %s %s" % (pycharm_path, linenumber, filename)
-#fp = open('/tmp/script.log', 'w')
-#fp.writelines([cmd])
-#fp.close()
-#print(cmd)
+#debug(cmd)
 
 # open
 run([cmd], shell=True)
